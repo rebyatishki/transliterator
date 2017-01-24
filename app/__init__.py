66e09980ed1d2cc5f319cf3f0ee11326a4380937
OlegUshakov <r3ady2di3@mail.ru>
2017-01-24 21:29:51 +0200

    serve static

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 from app import routes
